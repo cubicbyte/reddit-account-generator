64b385c023af7246882c76f712823e67cfd84a85
Bohdan Marukhnenko <bmaruhnenko@gmail.com>
2023-07-28 12:11:35 +0300

    Added automatic ffmpeg installation

--- reddit_account_generator/__init__.py
+++ reddit_account_generator/__init__.py
@@ -1,7 +1,16 @@
+# Download necessary ffmpeg binary
+import static_ffmpeg
+static_ffmpeg.add_paths()
+del static_ffmpeg
+
 from .maker import create_account
 from .protector import protect_account
+
 
 def install_driver():
     """Install firefox driver binary."""
     import webdriverdownloader
-    webdriverdownloader.GeckoDriverDownloader().download_and_install()
+    try:
+        webdriverdownloader.GeckoDriverDownloader().download_and_install()
+    except RuntimeError:
+        raise RuntimeError('Failed to install firefox driver. You\'re probably running the application too many times. Try again later. Limit: 60 requests per hour')
