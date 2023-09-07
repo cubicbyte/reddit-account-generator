0947730cdf14d569d833593911f926d00edd272e
Bohdan Marukhnenko <bmaruhnenko@gmail.com>
2023-09-07 19:14:05 +0300

    Added IPCooldownException.cooldown property

--- reddit_account_generator/exceptions.py
+++ reddit_account_generator/exceptions.py
@@ -1,5 +1,7 @@
 """General exceptions for the reddit_account_generator package."""
 
+from datetime import timedelta
+
 from selenium.common.exceptions import TimeoutException
 from selenium_recaptcha_solver.exceptions import RecaptchaException
 
@@ -32,6 +34,23 @@ class AuthException(RedditException):
     pass
 
 class IPCooldownException(RedditException):
-    pass
+    @property
+    def cooldown(self) -> timedelta:
+        """Cooldown in minutes."""
+        start = self.args[0].index('break for ') + len('break for ')
+        end = self.args[0].index(' ', start)
+
+        try:
+            cooldown = int(self.args[0][start:end]) + 1
+        except ValueError:
+            raise ValueError('Failed to parse cooldown from IPCooldownException')
+
+        if 'minutes' in self.args[0]:
+            return timedelta(minutes=cooldown)
+        elif 'seconds' in self.args[0]:
+            return timedelta(seconds=cooldown)
+        else:
+            raise ValueError('Failed to parse cooldown from IPCooldownException')
+
 
 NetworkException = (RecaptchaException, IPCooldownException, TimeoutException)
